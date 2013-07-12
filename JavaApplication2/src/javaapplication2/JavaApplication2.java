50b6b7ed352cd0500ceed62970aa5ddba2f0e84a
francesco-donato <francesco.donato.vr@gmail.com>
2013-07-12 11:50:26 +0200

    prova modifica di un non proprietario

--- JavaApplication2/src/javaapplication2/JavaApplication2.java
+++ JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -15,6 +15,6 @@ public class JavaApplication2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("HelloWorld, Ciao dona come stai?? Ma sei un po maiale");
+        System.out.println("HelloWorld, Ciao dona come stai?? Ma sei un po maiale. ahahahah");
     }
 }
